dc8ab875870c83c2e5e562c8141ce1e87193f659
Koki <kgoudy64@gmail.com>
2022-07-02 20:40:51 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int studentAge = 37;
+        double studentGPA = 2.9;
+        boolean hasPerfectAttendance = true;
+
+        String studentFirstName = "Koki";
+        String studentLastName = "Goudy";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(hasPerfectAttendance);
+    }
+}
