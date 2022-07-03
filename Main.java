7e2244078637ba2889b5e914fc55b70384520e1c
Koki <kgoudy64@gmail.com>
2022-07-02 21:23:59 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    
+    //create variables
     public static void main(String[] args) {
         int studentAge = 37;
         double studentGPA = 2.9;
@@ -16,9 +16,11 @@ public class Main {
         System.out.println(studentGPA);
         System.out.println(hasPerfectAttendance);
 
+        //concatenate variables
         System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
         System.out.println("What would you like to update it to? ");
 
+        //use Scanner to input new variable data
         Scanner input = new Scanner(System.in);
         studentGPA = input.nextDouble();
 
